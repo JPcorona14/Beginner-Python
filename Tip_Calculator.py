0fa6b03c6b8f0464eae9451a50ff4ae16530ce98
J <68132537+JPcorona14@users.noreply.github.com>
2021-02-10 14:58:52 -0800

    Update Tip_Calculator.py

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -1,1 +1,7 @@
+print("Welcome to the tip calculator!")
 
+bill = float(input("How much is the bill? $"))
+people = int(input("How many people will it be split through?"))
+tip = float(input("How much will the tip be? 10, 12, or 15?")) * 0.01
+
+print(f"Each person should pay ${str(round(((bill * tip) + bill) / people ,2))}")
