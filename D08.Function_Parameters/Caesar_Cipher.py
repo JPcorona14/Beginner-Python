acbf27dafa8b3f6af8a7b8bd71a50cb75f2c7138
J <68132537+JPcorona14@users.noreply.github.com>
2021-02-17 23:11:01 -0800

    Update to adjust for a shift higher than 26

--- D08.Function_Parameters/Caesar_Cipher.py
+++ D08.Function_Parameters/Caesar_Cipher.py
@@ -11,11 +11,9 @@ def caesar(start_text, shift_amount, cipher_direction):
     position = alphabet.index(char)
     new_position = position + shift_amount
     if new_position > 26:
-        while new_position > 26:
-            new_position -= 26
+        new_position -= 26
     elif new_position < 1:
-        while new_position < 1:
-            new_position += 26
+        new_position += 26
     end_text += alphabet[new_position]
     
   print(f"Here's the {cipher_direction}d result: {end_text}")
@@ -28,7 +26,7 @@ while code:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-
+    shift = shift % 26
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
     continued = input("Would you like to code again: Yes / No\n").lower()
     if continued == "no":
